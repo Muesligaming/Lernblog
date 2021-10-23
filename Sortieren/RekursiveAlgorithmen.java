5958d54addc34ae296618e8d7e93fb817ec895c5
Samuele Bradke <samu.bradke@gmail.com>
2021-10-23 15:20:21 +0200

    Mergesort

---
+++ Sortieren/RekursiveAlgorithmen.java
@@ -0,0 +1,225 @@
+/**
+ * Beschreibung
+ *
+ * @version 1.0 vom 24.08.2021
+ * @author Benjamin Reichelt
+ */
+import java.util.Random;
+
+import javax.swing.*;
+
+
+public class RekursiveAlgorithmen {
+  // Anfang Attribute
+  private int[] zahlen;
+
+  // Ende Attribute
+
+  /*
+   * Erzeugt ein neues Array der L?nge 50 und gelebt es mit ganzen Zufallszahlen
+   * zwischen 1 und 100.
+   */
+  public RekursiveAlgorithmen() {
+    erzeugeNeuesArray(50, 1, 100);
+  }
+
+  public void erzeugeNeuesArray(int pAnzahl, int pMinWert, int pMaxWert) {
+    zahlen = new int[pAnzahl];
+
+    Random zufallsgenerator = new Random();
+
+    // Array mit Zufallszahlen belegen
+    for (int j = 0; j < zahlen.length; j++) {
+      zahlen[j] = zufallsgenerator.nextInt(pMaxWert - pMinWert + 1) + pMinWert;
+    }
+  }
+
+  /*
+   * Pr?ft, ob das Array zahlen an Position pStelle die gesuchte Zahl pGesZahl
+   * enth?lt. Gibt true zur?ck, falls pStelle == -1.
+   */
+  public boolean pruefeWert(int pGesZahl, int pStelle) {
+    if (pStelle == -1) {
+      return true;
+    } else {
+      if (zahlen[pStelle] == pGesZahl) {
+        return true;
+      } else {
+        return false;
+      } // end of if-else
+    } // end of if-else
+  }
+
+  /* Gibt die Elemente des Arrays als String aus, jeweils 10 Zahlen pro Zeile */
+  public String arrayAusgeben() {
+    String res = zahlen[0] + "\t";
+
+    for (int i = 1; i < zahlen.length; i++) {
+      if ((i % 10) == 0) {
+        res = res + "\n";
+      } // end of if
+
+      res = res + zahlen[i] + "\t";
+    } // end of for
+
+    return res;
+  }
+
+  // Anfang Methoden
+  public int binSucheRek(int pGesZahl) {
+    return binSucheRek(pGesZahl, 0, this.zahlen.length - 1);
+  }
+
+  private int binSucheRek(int pGesZahl, int pLPos, int pRPos) {
+    int mitte = (pLPos + pRPos) / 2;
+
+    // Rekursionsbasis
+    // Fall: Gesuchte Zahl befindet sich an mittlerer Position des Suchbereichs
+    if (zahlen[mitte] == pGesZahl) {
+      return mitte;
+    } // end of if
+
+    // Fall: Suchbereich enth?lt nur ein Element, das nicht die gesuchte Zahl ist
+    if (pLPos == pRPos) {
+      return -1;
+    } // end of if-else
+
+    // Rekursionsschritt
+    else {
+      // Fall: Gesuchte Zahl ist gr??er als Zahl in der Mitte des Suchbereichs
+      if (pGesZahl > zahlen[mitte]) {
+        // Rekursiver Aufruf auf rechte H?lfte des Suchbereichs
+        return binSucheRek(pGesZahl, mitte + 1, pRPos);
+      } // end of if
+      else {
+        // Rekursiver Aufruf auf linke H?lfte des Suchbereichs
+        return binSucheRek(pGesZahl, pLPos, mitte - 1);
+      } // end of if-else
+    } // end of if-else
+  }
+
+  // Elementare Sortierverfahren
+
+  // Bubblesort rekursiv
+  public void bubblesortRek() {
+    bubblesortRek(zahlen.length);
+  }
+
+  // Bubblesort rekursiv
+  private void bubblesortRek(int pEndIndex) { // pEndIndex markiert das Ende des unsortierten Bereichs    
+  
+    if (pEndIndex == 0) {
+    } else {
+      for (int j = 0; j < (pEndIndex - 1); j++) {
+        if (zahlen[j] > zahlen[j + 1]) {
+          int temp = zahlen[j];
+          zahlen[j] = zahlen[j + 1];
+          zahlen[j + 1] = temp;
+        }
+      }
+  
+      bubblesortRek(pEndIndex - 1);
+    } // end of if-else
+  }
+
+  // Insertionsort rekursiv
+  public void insertionSortRek() {
+    insertionSort(zahlen.length);
+  }
+
+  public void Mergesort() {
+    Mergesort(0, zahlen.length);
+  }
+
+  private void Mergesort(int Pl, int Pr) {
+    if (Pl < Pr) {
+      int mitte = (Pl + Pr) / 2;
+      Mergesort(Pl, mitte);
+      Mergesort(mitte + 1, Pr);
+      merge(Pl, Pr);
+
+      return;
+    } // end of if
+
+    return;
+  }
+
+  private void merge(int pLow, int pHigh) {
+    int center = (pLow + pHigh) / 2;
+    int[] tempA = new int[(center + 1) - pLow];
+    int temp = 0;
+
+    for (int index = pLow; index <= center; index++) {
+      tempA[temp] = zahlen[index];
+      temp++;
+    } // end of for
+
+    int l = pLow;
+    int r = center + 1;
+    temp = 0;
+
+    while ((l < r) && (r <= pHigh)) {
+      if (tempA[temp] <= zahlen[r]) {
+        zahlen[l] = tempA[temp];
+        l++;
+        temp++;
+      } else {
+        zahlen[l] = zahlen[r];
+        l++;
+        r++;
+      } // end of if-else      
+    } // end of while
+
+    while (temp < tempA.length) {
+      zahlen[l] = tempA[temp];
+      l++;
+      temp++;
+    } // end of while 
+    return;
+  }
+
+  private void insertionSort(int pEndIndex) { // pEndIndex markiert das Ende des zu sortierenen Bereichs
+  
+    if (!(pEndIndex < 1)) {
+      //zu sortierender Bereich besteht nur aus einem Element => ist schon sortiert
+      insertionSort(pEndIndex - 1); //zu sortierenden Bereich wird um eins verkleinert und sortiert 
+  
+      int value = zahlen[pEndIndex - 1]; //Zahl die eingefügt werden soll
+      int pos = pEndIndex - 2; //setzt die positzion des ersten zu vergleichenen Arry indexes
+  
+      while ((pos >= 0) && (zahlen[pos] > value)) { // alle Elemente nach rechts schieben 
+        zahlen[pos + 1] = zahlen[pos]; // bis die Richtige Position für den Wert gefunden ist
+        pos--;
+      }
+  
+      zahlen[pos + 1] = value; //setze den einzusetzenden Wert an die richtige Position
+    }
+  }
+
+  // Selectionsort (Minsort) rekursiv
+  public void selectionsSortRek() {
+    selectionsortRek(0);
+  }
+
+  public void selectionsortRek(int pStartIndex) { // pStartIndex markiert den Beginn des unsortierten Bereichs
+  
+    if (pStartIndex > (zahlen.length - 1)) {
+    } else {
+      int low = pStartIndex;
+  
+      for (int i = pStartIndex + 1; i < zahlen.length; i++) {
+        if (zahlen[i] < zahlen[low]) {
+          low = i;
+        }
+      }
+  
+      int temporer = zahlen[low];
+      zahlen[low] = zahlen[pStartIndex];
+      zahlen[pStartIndex] = temporer;
+  
+      selectionsortRek(pStartIndex + 1);
+    } // end of if-else
+  }
+
+  // Ende Methoden
+} // end of Suche
---
+++ Sortieren/RekursiveAlgorithmenGUI.java
@@ -0,0 +1,212 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 31.08.2021
+ * @author Benjamin Reichelt
+ */
+public class RekursiveAlgorithmenGUI extends JFrame {
+  // Anfang Attribute
+  private JLabel jLabel1 = new JLabel();
+  private JTextArea jtxtAusgabe = new JTextArea("");
+  private JScrollPane jtxtAusgabeScrollPane = new JScrollPane(jtxtAusgabe);
+  private JButton btnSuche = new JButton();
+  private JLabel jLabel2 = new JLabel();
+  private JNumberField jnbfZahl = new JNumberField();
+  private JButton btnErzeugeArray = new JButton();
+  private RekursiveAlgorithmen algo = new RekursiveAlgorithmen();
+  private JLabel lblAusgabe = new JLabel();
+  private JLabel jlblNBild = new JLabel();
+  private ImageIcon kreuz = new ImageIcon("images/Kreuz.PNG");
+  private ImageIcon haken = new ImageIcon("images/Haken.PNG");
+  private JButton btnSelectionsort = new JButton();
+  private JButton btnInsertionssort = new JButton();
+  private JButton btnBubblesort = new JButton();
+  private JButton btnMergesort = new JButton();
+  
+
+  // Ende Attribute
+  public RekursiveAlgorithmenGUI(String title) {
+    // Frame-Initialisierung
+    super(title);
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+    int frameWidth = 513;
+    int frameHeight = 499;
+    setSize(frameWidth, frameHeight);
+
+    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    int x = (d.width - getSize().width) / 2;
+    int y = (d.height - getSize().height) / 2;
+    setLocation(x, y);
+    setResizable(false);
+
+    Container cp = getContentPane();
+    cp.setLayout(null);
+    // Anfang Komponenten
+    jLabel1.setBounds(8, 8, 482, 33);
+    jLabel1.setText("Rekursive Algorithmen");
+    jLabel1.setFont(new Font("Dialog", Font.BOLD, 24));
+    jLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+    cp.add(jLabel1);
+    jtxtAusgabeScrollPane.setBounds(8, 48, 481, 177);
+    jtxtAusgabe.setEnabled(true);
+    jtxtAusgabe.setEditable(false);
+    jtxtAusgabe.setTabSize(3);
+    jtxtAusgabe.setFont(new Font("Dialog", Font.BOLD, 16));
+    cp.add(jtxtAusgabeScrollPane);
+    btnSuche.setBounds(128, 232, 81, 33);
+    btnSuche.setText("Suche");
+    btnSuche.setMargin(new Insets(2, 2, 2, 2));
+    btnSuche.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnSuche_ActionPerformed(evt);
+        }
+      });
+    btnSuche.setEnabled(false);
+    cp.add(btnSuche);
+    jLabel2.setBounds(216, 232, 43, 33);
+    jLabel2.setText("Zahl:");
+    jLabel2.setFont(new Font("Dialog", Font.BOLD, 16));
+    cp.add(jLabel2);
+    jnbfZahl.setBounds(264, 232, 41, 33);
+    jnbfZahl.setText("");
+    jnbfZahl.setFont(new Font("Dialog", Font.PLAIN, 16));
+    jnbfZahl.setEnabled(false);
+    jnbfZahl.setHorizontalAlignment(SwingConstants.CENTER);
+    cp.add(jnbfZahl);
+    btnErzeugeArray.setBounds(8, 232, 105, 33);
+    btnErzeugeArray.setText("Erzeuge Werte");
+    btnErzeugeArray.setMargin(new Insets(2, 2, 2, 2));
+    btnErzeugeArray.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnErzeugeArray_ActionPerformed(evt);
+        }
+      });
+    cp.add(btnErzeugeArray);
+    lblAusgabe.setBounds(8, 272, 395, 49);
+    lblAusgabe.setText("");
+    lblAusgabe.setHorizontalAlignment(SwingConstants.LEFT);
+    lblAusgabe.setFont(new Font("Dialog", Font.BOLD, 14));
+    cp.add(lblAusgabe);
+    jlblNBild.setBounds(416, 264, 60, 60);
+    jlblNBild.setText("");
+    jlblNBild.setIcon(null);
+    cp.add(jlblNBild);
+    btnSelectionsort.setBounds(8, 416, 145, 33);
+    btnSelectionsort.setText("Selectionsort");
+    btnSelectionsort.setMargin(new Insets(2, 2, 2, 2));
+    btnSelectionsort.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnSelectionsort_ActionPerformed(evt);
+        }
+      });
+    cp.add(btnSelectionsort);
+    btnInsertionssort.setBounds(8, 376, 145, 33);
+    btnInsertionssort.setText("Insertionsort");
+    btnInsertionssort.setMargin(new Insets(2, 2, 2, 2));
+    btnInsertionssort.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnInsertionssort_ActionPerformed(evt);
+        }
+      });
+    cp.add(btnInsertionssort);
+    btnBubblesort.setBounds(8, 336, 145, 33);
+    btnBubblesort.setText("Bubblesort");
+    btnBubblesort.setMargin(new Insets(2, 2, 2, 2));
+    btnBubblesort.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnBubblesort_ActionPerformed(evt);
+        }
+      });
+    cp.add(btnBubblesort);
+    btnMergesort.setBounds(160, 336, 145, 33);
+    btnMergesort.setText("Mergesort");
+    btnMergesort.setMargin(new Insets(2, 2, 2, 2));
+    btnMergesort.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+          btnMergesort_ActionPerformed(evt);
+        }
+      });
+    cp.add(btnMergesort);
+    // Ende Komponenten
+    setVisible(true);
+  } // end of public BinaereSuche
+
+  // Anfang Methoden
+  public static void main(String[] args) {
+    new RekursiveAlgorithmenGUI("Rekursive Algorithmen");
+  } // end of main
+
+  public void btnSuche_ActionPerformed(ActionEvent evt) {
+    int eingabe = jnbfZahl.getInt();
+    int stelle = algo.binSucheRek(eingabe);
+
+    if (stelle != -1) {
+      lblAusgabe.setText("Die gesuchte Zahl befindet sich an der Stelle: " +
+                         stelle + ".");
+      jlblNBild.setIcon(haken);
+    } else {
+      lblAusgabe.setText("Die gesuchte Zahl ist im Array nicht enthalten!");
+      jlblNBild.setIcon(kreuz);
+    } // end of if-else
+  } // end of btnSuche_ActionPerformed
+
+  public void btnErzeugeArray_ActionPerformed(ActionEvent evt) {
+    algo.erzeugeNeuesArray(40, 0, 100);
+    jtxtAusgabe.setText(algo.arrayAusgeben());
+    lblAusgabe.setText("");
+    jnbfZahl.setInt(0);
+    jlblNBild.setIcon(null);
+    btnSuche.setEnabled(true);
+    jnbfZahl.setEnabled(true);
+  } // end of btnErzeugeArray_ActionPerformed
+
+  public void btnBubblesort_ActionPerformed(ActionEvent evt) {
+    algo.bubblesortRek();
+    jtxtAusgabe.setText(algo.arrayAusgeben());
+    lblAusgabe.setText("");
+    jnbfZahl.setInt(0);
+    jlblNBild.setIcon(null);
+    btnSuche.setEnabled(true);
+    jnbfZahl.setEnabled(true);
+  } // end of btnBubblesort_ActionPerformed
+
+  public void btnInsertionssort_ActionPerformed(ActionEvent evt) {
+    algo.insertionSortRek();
+    jtxtAusgabe.setText(algo.arrayAusgeben());
+    lblAusgabe.setText("");
+    jnbfZahl.setInt(0);
+    jlblNBild.setIcon(null);
+    btnSuche.setEnabled(true);
+    jnbfZahl.setEnabled(true);
+  } // end of btnInsertionssort_ActionPerformed
+
+  public void btnSelectionsort_ActionPerformed(ActionEvent evt) {
+    algo.selectionsSortRek();
+    jtxtAusgabe.setText(algo.arrayAusgeben());
+    lblAusgabe.setText("");
+    jnbfZahl.setInt(0);
+    jlblNBild.setIcon(null);
+    btnSuche.setEnabled(true);
+    jnbfZahl.setEnabled(true);
+  } // end of btnSelectionsort_ActionPerformed
+  
+  public void btnMergesort_ActionPerformed(ActionEvent evt) {
+    algo.Mergesort();
+    jtxtAusgabe.setText(algo.arrayAusgeben());
+    lblAusgabe.setText("");
+    jnbfZahl.setInt(0);
+    jlblNBild.setIcon(null);
+    btnSuche.setEnabled(true);
+    jnbfZahl.setEnabled(true);    
+  } 
+  // Ende Methoden
+} // end of class BinaereSuche
